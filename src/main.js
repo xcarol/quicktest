305e2ac197908bc45224eb9b1fcc7008f71cbb8a
xcarol <xcarolr@gmail.com>
2024-11-25 18:32:04 +0100

    use cookies to persist pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import VueCookies from 'vue-cookies';
 
 import App from './App.vue';
 import router from './router';
@@ -9,6 +10,7 @@ import vuetify from './plugins/vuetify';
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(VueCookies);
 app.use(router);
 app.use(i18n);
 app.use(vuetify);
